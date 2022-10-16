d6d2cf08ecb51e8ef3ca5bb8bef5299a93897aaf
costa-rica <nrodrig1@gmail.com>
2022-10-16 08:00:40 +0200

    recreated problem with solution commented below

--- run.py
+++ run.py
@@ -4,4 +4,10 @@ import pandas as pd
 dataset = {'id': [1], 'creationDate':['2022-10-15'], 'value':['5']}
 
 df=pd.DataFrame(data=dataset)
-df.to_sql('apple_health_export', engine, if_exists='append', index=False)
+
+# Here is the problem
+df.to_sql('Apple_health_export', engine, if_exists='append', index=False)
+
+# replacing this line with the lowercase 'apple_health_export' solves the problem
+# this needs to match what i have in models02.py: __tablename__ = 'apple_health_export'
+# df.to_sql('apple_health_export', engine, if_exists='append', index=False)
